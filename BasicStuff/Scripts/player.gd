5dc4550003c4e5b31e7f4b4db67b56b303780ebe
Gabbagool <37012589+Gabbagoool@users.noreply.github.com>
2026-08-31 20:54:58 +0200

    tp buttons in trainroom
    there now appears three teleport buttons when you walk into the train room, the first one works, the second one almost works, the last one does nothing

--- BasicStuff/Scripts/player.gd
+++ BasicStuff/Scripts/player.gd
@@ -7,6 +7,7 @@ var dashCD = 0
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED	
+	$Canvas/MarginContainer/HBoxContainer.visible = false
 
 
 func _input(event: InputEvent) -> void:	
@@ -72,3 +73,32 @@ func _physics_process(delta: float) -> void:
 		else:
 			velocity.y -= 9.81 * delta
 	move_and_slide()
+
+var telPos : Vector3
+var x
+var y
+var z
+func _on_button_1_pressed() -> void:
+	x = ($"../TrainStation".scale.x * 5) / 2
+	y = 2
+	z = ($"../TrainStation".scale.z * 10) / 2 
+	
+	var rotr =  $"../TrainStation".global_transform.basis.orthonormalized()
+	telPos = rotr * Vector3(x,y,z) + $"../TrainStation".global_position
+	
+	position = telPos
+
+func _on_button_2_pressed() -> void:
+	
+	x = ($"../Environment/randwall3".scale.x) / 2
+	y = ($"../Environment/randwall3".scale.y * 2.885)
+	z = ($"../Environment/randwall3".scale.z) / 2
+	
+	var rotr =  $"../Environment/randwall3".global_transform.basis.orthonormalized()
+	telPos = rotr * Vector3(x,y,z) + $"../Environment/randwall3".global_position
+	
+	position = telPos
+	
+	#position.x = $"../Environment/randwall3".position.x + ($"../Environment/randwall3".scale.x) / 2
+	#position.y = $"../Environment/randwall3".position.y + ($"../Environment/randwall3".scale.y * 2.9) / 2
+	#position.z = $"../Environment/randwall3".position.z + ($"../Environment/randwall3".scale.z) / 2 
--- BasicStuff/Scripts/train_room.gd
+++ BasicStuff/Scripts/train_room.gd
@@ -6,8 +6,12 @@ func _ready() -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is player:
 		$"../Environment/DirectionalLight3D".visible = false
-		pass # Replace with function body.
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE 
+		$"../Player/Canvas/MarginContainer/HBoxContainer".visible = true
+		
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
 	$"../Environment/DirectionalLight3D".visible = true
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$"../Player/Canvas/MarginContainer/HBoxContainer".visible = false
+	
