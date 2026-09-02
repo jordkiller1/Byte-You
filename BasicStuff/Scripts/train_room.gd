1d478797d75f9e9f9a425896fd52a67aede235f1
Gabbagool <37012589+Gabbagoool@users.noreply.github.com>
2026-09-02 21:33:05 +0200

    Fight room design + light revamp
    Added walls into the fight room.
    Added and changed position of fightroom lights.
    Revamped light graphics with use of environment node
    removed coin in fight room
    Altered gray texture to be more metallic, like trains
    doors use the wood texture just because.

--- BasicStuff/Scripts/train_room.gd
+++ BasicStuff/Scripts/train_room.gd
@@ -21,8 +21,10 @@ func _on_area_3d_body_exited(body: Node3D) -> void:
 
 func onDoorTouch1(body: Node3D) -> void:
 	if body is player:
-		$"../Player".position = $"../roomFight/doorSpawn/tPP".global_position
+		$"../Player".position = $"../roomFight/Room/doorSpawn/tPP".global_position
 	pass # Replace with function body.
 
 func onDoorTouch2(body: Node3D) -> void:
+	if body is player:
+		$"../Player".position = $"../roomPuzzle/doorSpawn/tPP".global_position
 	pass # Replace with function body.
